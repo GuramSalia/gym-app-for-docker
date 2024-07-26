a8988c95660c8bc5a6a597e0df02d6c9bdd59139
Guram Salia <guramsalia@gmail.com>
2024-07-26 18:02:10 +0400

    comment out jms related import

--- src/main/java/com/epam/gym_app/GymAppApplication.java
+++ src/main/java/com/epam/gym_app/GymAppApplication.java
@@ -2,7 +2,7 @@ package com.epam.gym_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
+//import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 //@EnableJms
